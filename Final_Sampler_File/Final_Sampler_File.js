5f42e720fb60b9e3af19050977867b0e7501c056
kaylainsyira <83621348+kaylainsyira@users.noreply.github.com>
2021-06-02 18:09:32 +0700

    Update Final_Sampler_File.js

--- Final_Sampler_File/Final_Sampler_File.js
+++ Final_Sampler_File/Final_Sampler_File.js
@@ -1,6 +1,6 @@
 var Juliet;
 var Supply;
-let sentence = ['For never', 'was a story of', 'more woe than', 'this of Juliet', 'and her Romeo', '"FOR NEVER WAS A STORY OF MORE WOE THAN THIS OF JULIET AND HER ROMEO."'];
+let sentence = ['Love is', 'heavy & light', 'bright & dark', 'asleep & awake', 'Its everything except what it is!', 'For never', 'was a story of', 'more woe', 'than this of', 'JULIET AND HER ROMEO'];
 let word;
 let ts = 100;
 let speed = 5;
@@ -10,22 +10,24 @@ let polySynth;
 function preload() {
   Juliet = loadFont('data/Juliet-Serif.otf');
   Supply = loadFont('data/Supply-Regular.otf');
+  song = loadSound('data/Sound-RJ.mp3');
 }
 
 function setup() {
-  let cnv = createCanvas(windowWidth, windowHeight);
-  cnv.mouseClicked(playSynth); //how to change it to follow the word change?
+  createCanvas(windowWidth, windowHeight);
   textFont(Juliet);
   textAlign(CENTER, CENTER);
   word = sentence[0];
-  
-  polySynth = new p5.PolySynth();
 
-  setTimeout(one, 1000);
-  setTimeout(two, 2000);
-  setTimeout(three, 3000);
-  setTimeout(four, 4000);
-  setTimeout(five, 6000);
+  setTimeout(one, 2000);
+  setTimeout(two, 3000);
+  setTimeout(three, 4000);
+  setTimeout(four, 5000);
+  setTimeout(five, 8500);
+  setTimeout(six, 10000);
+  setTimeout(seven, 14000);
+  setTimeout(eight, 16000);
+  setTimeout(nine, 18000);
 }
 
 function one(){
@@ -36,25 +38,49 @@ function one(){
 
 function two(){
   word = sentence[2];
-  ts = 175;
+  ts = 100;
   speed = 5;
 }
 
 function three(){
   word = sentence[3];
-  ts = 150;
+  ts = 100;
   speed = 5;
 }
 
 function four(){
   word = sentence[4];
-  ts = 150;
+  ts = 100;
   speed = 5;
 }
 
 function five(){
   word = sentence[5];
-  ts = 105;
+  ts = 150;
+  speed = 5;
+}
+
+function six(){
+  word = sentence[6];
+  ts = 100;
+  speed = 5;
+}
+
+function seven(){
+  word = sentence[7];
+  ts = 150;
+  speed = 5;
+}
+
+function eight(){
+  word = sentence[8];
+  ts = 100;
+  speed = 5;
+}
+
+function nine(){
+  word = sentence[9];
+  ts = 150;
   speed = 5;
 }
 
@@ -63,6 +89,8 @@ function draw() {
   textFont(Juliet)
   textSize(ts);
   text(word, 0.15*width, 0.15*height, 0.7*width, 0.7*height);
+  
+  song.setVolume(map(mouseX, 0, width, 0, 1));
   
   fill(0);
   noStroke();
@@ -74,29 +102,10 @@ function draw() {
   noStroke();
   textFont(Supply);
   textSize(9);
-  text('click anywhere for sound', width/2, 0.075*height);
+  text('click anywhere for sound, move for volume', width/2, 0.075*height);
   
 }
 
-function playSynth() {
-  userStartAudio();
-
-  // note duration (in seconds)
-  let dur = 0.5;
-
-  // time from now (in seconds)
-  let time = 1;
-
-  // velocity (volume, from 0 to 1)
-  let vel = 0.3;
-
-  // notes can overlap with each other
-  polySynth.play('C5', vel, time*1, dur);
-  polySynth.play('E5', vel, time*1, dur);
-  polySynth.play('G5', vel, time*1, dur);
-  polySynth.play('C3', vel, time*2, dur);
-  polySynth.play('A5', vel, time*3, dur);
-  polySynth.play('C5', vel, time*4, dur);
-  polySynth.play('E5', vel, time*4, dur);
-  polySynth.play('A5', vel, time*4, dur);
+function mousePressed() {
+  song.play();
 }
